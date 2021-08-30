7a9115a696a1a6c2875ebaf209f0bd7870c1ac76
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-08-30 21:26:13 +0300

    finish_lab12

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # caps
 
-## LAB - 11
+## LAB - 12
 
 ### Deployment Test
 
 **Author: Tariq Abu-Laban**
 
 - [tests report](https://github.com/Abu-laban/caps/actions).
-- [pull request](https://github.com/Abu-laban/caps/pull/1).
+- [pull request](https://github.com/Abu-laban/caps/pull/2).
 
 **Setup**
 
 `.env` **requirements**
 
-- `STORE` = 1-206-flowers
+- `PORT` = `3000`
+
+- `STORE` = `1-206-flowers`
+
+- `HOST` = `http://localhost:3000`
 
 **Running the app**
 
@@ -21,15 +25,15 @@
 
   - Returns Object
 
-  ![caps](./caps.PNG)
+  ![caps](./socket.io.PNG)
 
 **Tests**
 
 - Unit Tests: `npm run test`
 - Lint Tests: `npm run lint`
 
-![test](./test.PNG)
+![test](./test1.PNG)
 
 **UML**
 
-![uml](uml.png)
+![uml](UML1.png)
--- __test__/models.test.js
+++ __test__/models.test.js
@@ -1,59 +1,91 @@
 'use strict';
 
-const events = require('../events');
-const supertest = require('supertest');
-
+const caps = require('../CAPS/caps.js');
 
 let payload = {
-    store: '1-206-flowers',
-    orderID: '4959e85d-1e95-48f9-bbd0-25e588e05b77',
-    customer: 'Edgar Barton',
-    address: '10808 Gutkowski Heights'
+    storeId: '1-206-flowers',
+    orderID: 'e3669048-7313-427b-b6cc-74010ca1f8f0',
+    customer: 'Jamal Braun',
+    address: 'Schmittfort, LA'
 };
 
-jest.useFakeTimers();
+describe('testing event handlers', () => {
+    let consoleSpy;
 
-describe('caps test', () => {
+    beforeAll(() => {
+        consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+    });
 
-    it('pickup', () => {
-        const caps = require('../src/caps');
+    it('pickup Work', async () => {
         caps.emit('pickup', payload);
-        expect(caps.emit('pickup', payload)).toEqual(true);
+        await consoleSpy();
+        expect(consoleSpy).toHaveBeenCalled();
     });
 
-    it('in-transit', () => {
-        const caps = require('../src/caps');
+    it('in-transit  Work ', async () => {
         caps.emit('in-transit', payload);
-        expect(caps.emit('in-transit', payload)).toEqual(true);
+        await consoleSpy();
+        expect(consoleSpy).toHaveBeenCalled();
     });
 
-    it('delivered', () => {
-        const caps = require('../src/caps');
+    it('delivered  Work  ', async () => {
         caps.emit('delivered', payload);
-        expect(caps.emit('delivered', payload)).toEqual(true);
+        await consoleSpy();
+        expect(consoleSpy).toHaveBeenCalled();
     });
 
+    describe('caps test', () => {
+
+        it('pickup', () => {
+            const caps = require('../CAPS/caps');
+            caps.emit('pickup', payload);
+            expect(caps.emit('pickup', payload)).toEqual(true);
+        });
+
+        it('in-transit', () => {
+            const caps = require('../CAPS/caps');
+            caps.emit('in-transit', payload);
+            expect(caps.emit('in-transit', payload)).toEqual(true);
+        });
+
+        it('delivered', () => {
+            const caps = require('../CAPS/caps');
+            caps.emit('delivered', payload);
+            expect(caps.emit('delivered', payload)).toEqual(true);
+        });
+
+
+    });
+
+    describe('driver test', () => {
+
+        it('pickup', async () => {
+            const driver = require('../Driver/driver');
+            driver.emit('pickup', payload);
+            await consoleSpy();
+            expect(consoleSpy).toHaveBeenCalled();
+        });
+
+    });
+
+
+
+    describe('vendor test', () => {
+        it('delivered', async () => {
+            const vendor = require('../Vendor/vendor');
+            vendor.emit('delivered', payload);
+            await consoleSpy();
+            expect(consoleSpy).toHaveBeenCalled();
+        });
+
+    });
+    afterAll(async () => {
+        consoleSpy.mockRestore();
+
+    });
 
 });
 
 
-describe('driver test', () => {
-
-    it('pickup', () => {
-        const driver = require('../src/models/Drivers/driver');
-        driver.emit('pickup', payload);
-        expect(driver.emit('pickup', payload)).toEqual(true);
-    });
-
-});
 
 
-
-describe('vendor test', () => {
-    it('delivered', () => {
-        const vendor = require('../src/models/Vendor/vendor');
-        vendor.emit('delivered', payload);
-        expect(vendor.emit('delivered', payload)).toEqual(true);
-    });
-
-});
